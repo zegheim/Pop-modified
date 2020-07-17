e0d8a4ad9d70a5df456dee330d31b5a98b108f26
zegheim <justinarista@gmail.com>
2020-07-17 21:27:14 +0700

    update extract.sh to work with latest workflow

--- utils/extract.sh
+++ utils/extract.sh
@@ -7,13 +7,14 @@ for r in `gresource list $GRESOURCE`; do
     gresource extract $GRESOURCE $r | install -D /dev/stdin  $WORKDIR/${r#\/org\/gnome\/shell/}
 done
 
-if [ ! -f "$WORKDIR/theme/gdm3-modified.css" ]; then
-    echo '@import url("resource:///org/gnome/shell/theme/gdm3.css");' > $WORKDIR/theme/gdm3-modified.css
-    chmod 665 $WORKDIR/theme/gdm3-modified.css
+
+if [ ! -f "$WORKDIR/theme/gdm3-original.css" ]; then
+    mv $WORKDIR/theme/gdm3.css $WORKDIR/theme/gdm3-original.css
+    echo '@import url("resource:///org/gnome/shell/theme/gdm3-original.css");' > $WORKDIR/theme/gdm3.css
 fi
 
-if [ ! -f "$WORKDIR/theme/gnome-shell-modified.css" ]; then
-    echo '@import url("resource:///org/gnome/shell/theme/gnome-shell.css");' > $WORKDIR/theme/gnome-shell-modified.css
-    chmod 665 $WORKDIR/theme/gnome-shell-modified.css
+if [ ! -f "$WORKDIR/theme/gnome-shell-original.css" ]; then
+    mv $WORKDIR/theme/gnome-shell.css $WORKDIR/theme/gnome-shell-original.css
+    echo '@import url("resource:///org/gnome/shell/theme/gnome-shell-original.css");' > $WORKDIR/theme/gnome-shell.css
 fi
 
