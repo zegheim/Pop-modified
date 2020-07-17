525bbc2a583a94d3cb4813122637bbc5ef4e85f6
zegheim <justinarista@gmail.com>
2020-07-17 11:09:40 +0700

    allow creation of non-existent directories in extract.sh

--- utils/extract.sh
+++ utils/extract.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-WORKDIR = ${HOME}/.local/share/gnome-shell/theme/Pop-modified
+WORKDIR=${HOME}/.local/share/gnome-shell/theme/Pop-modified
 GRESOURCE=${HOME}/.local/share/gnome-shell/theme/Pop-modified/gnome-shell-theme.gresource
 
 for r in `gresource list $GRESOURCE`; do
-    gresource extract $GRESOURCE $r > $WORKDIR/${r#\/org\/gnome\/shell/}
+    gresource extract $GRESOURCE $r | install -D /dev/stdin  $WORKDIR/${r#\/org\/gnome\/shell/}
 done
