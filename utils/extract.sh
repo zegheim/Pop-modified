1ca44195900c9d730da358ccfd3457998a3e2090
zegheim <justinarista@gmail.com>
2020-07-17 15:29:31 +0700

    do not apply changes directly on the file but import and add changes in a new file

--- utils/extract.sh
+++ utils/extract.sh
@@ -6,3 +6,9 @@ GRESOURCE=${WORKDIR}/gnome-shell-theme.gresource
 for r in `gresource list $GRESOURCE`; do
     gresource extract $GRESOURCE $r | install -D /dev/stdin  $WORKDIR/${r#\/org\/gnome\/shell/}
 done
+
+if [ ! -f "$WORKDIR/theme/original.css" ]; then
+    mv $WORKDIR/theme/gdm3.css $WORKDIR/theme/original.css
+    echo '@import url("resource:///org/gnome/shell/theme/original.css");' > $WORKDIR/theme/gdm3.css
+fi
+
