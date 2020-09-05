2bae351600c525839c6745f11d6900828e4e527f
zegheim <justinarista@gmail.com>
2020-09-05 11:20:37 +0100

    change desktop icon font color

---
+++ utils/desktop.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo cp /home/zegheim/.local/share/gnome-shell/theme/Pop-modified/desktop-icons@csoriano/stylesheet.css /usr/share/gnome-shell/extensions/desktop-icons@csoriano/stylesheet.css
