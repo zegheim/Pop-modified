30fa6b545c5d8f79b9e1a375ff9a06a9f846a614
zegheim <justin@aristakas.in>
2020-12-01 01:08:54 +0000

    add utility to backup existing .gresource file

---
+++ utils/backup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo cp /usr/share/gnome-shell/theme/Pop/gnome-shell-theme.gresource /home/zegheim/.local/share/gnome-shell/theme/Pop-modified/gnome-shell-theme.gresource /usr/share/gnome-shell/theme/Pop/gnome-shell-theme.gresource.backup
